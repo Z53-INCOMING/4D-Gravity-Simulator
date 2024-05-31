ba6260278cec92db83f49f1951b8417c6052874d
Z53-INCOMING <75651517+Z53-INCOMING@users.noreply.github.com>
2024-05-31 14:09:47 -0700

    Navigation update

--- advanced_3d_camera.gd
+++ advanced_3d_camera.gd
@@ -29,6 +29,12 @@ var enabled = true
 
 var resolution = Vector2(ProjectSettings.get_setting("display/window/size/viewport_width"), ProjectSettings.get_setting("display/window/size/viewport_height"))
 
+var following = false
+
+var follow_id = 0
+
+var input_timer = 0.0
+
 func _ready():
 	camera.position.z = zoom
 
@@ -62,21 +68,33 @@ func _process(delta):
 		if movement_allowed:
 			if Input.is_action_just_pressed("pan") and Input.is_action_pressed("shift"):
 				mouseStart = get_viewport().get_mouse_position()
+				following = false
 			
 			if Input.is_action_pressed("pan") and Input.is_action_pressed("shift"):
 				var mousePos = get_viewport().get_mouse_position()
-				var sensitivity = 0.001 * zoom
+				var sensitivity = 0.001 * zoom * (0.25 if Input.is_action_pressed("control") else 1.0)
 				
 				position.x += cos(rotation.y + PI) * (mousePos - mouseStart).x * sensitivity
 				position.z -= sin(rotation.y + PI) * (mousePos - mouseStart).x * sensitivity
 				
-				if Input.is_action_pressed("control"):
-					position.x += cos(rotation.y + PI*0.5) * (mousePos - mouseStart).y * sensitivity# * -sign(pivot.rotation.x)
-					position.z -= sin(rotation.y + PI*0.5) * (mousePos - mouseStart).y * sensitivity# * -sign(pivot.rotation.x)
-				else:
-					position.x -= cos(rotation.y + PI*0.5) * sin(pivot.rotation.x) * (mousePos - mouseStart).y * sensitivity
-					position.y += cos(pivot.rotation.x) * (mousePos - mouseStart).y * sensitivity
-					position.z += sin(rotation.y + PI*0.5) * sin(pivot.rotation.x) * (mousePos - mouseStart).y * sensitivity
+				position.x -= cos(rotation.y + PI*0.5) * sin(pivot.rotation.x) * (mousePos - mouseStart).y * sensitivity
+				position.y += cos(pivot.rotation.x) * (mousePos - mouseStart).y * sensitivity
+				position.z += sin(rotation.y + PI*0.5) * sin(pivot.rotation.x) * (mousePos - mouseStart).y * sensitivity
+				
+				mouseStart = get_viewport().get_mouse_position()
+			
+			if Input.is_action_just_pressed("4d look") and Input.is_action_pressed("shift"):
+				mouseStart = get_viewport().get_mouse_position()
+				following = false
+			
+			if Input.is_action_pressed("4d look") and Input.is_action_pressed("shift"):
+				var mousePos = get_viewport().get_mouse_position()
+				var sensitivity = 0.001 * zoom * (0.25 if Input.is_action_pressed("control") else 1.0)
+				
+				position.x += cos(rotation.y+PI*0.5) * (mousePos - mouseStart).y * sensitivity
+				position.z -= sin(rotation.y+PI*0.5) * (mousePos - mouseStart).y * sensitivity
+				
+				Globals.camera_w += (mousePos - mouseStart).x * -sensitivity
 				
 				mouseStart = get_viewport().get_mouse_position()
 		
@@ -92,16 +110,55 @@ func _process(delta):
 		
 		if Input.is_action_pressed("kata"):
 			Globals.camera_w -= speed * delta
+			following = false
 		if Input.is_action_pressed("ana"):
 			Globals.camera_w += speed * delta
+			following = false
 		
-		if Input.is_action_just_pressed("4d look"):
+		if Input.is_action_just_pressed("4d look") and !Input.is_action_pressed("shift"):
 			mouseStart = get_viewport().get_mouse_position()
 		
-		if Input.is_action_pressed("4d look"):
+		if Input.is_action_pressed("4d look") and !Input.is_action_pressed("shift"):
 			var mousePos = get_viewport().get_mouse_position()
 			
-			Globals.xw_angle += ((mousePos - mouseStart).x * mouseSensitivity)
-			Globals.zw_angle += ((mousePos - mouseStart).y * mouseSensitivity)
+			Globals.xw_angle += cos(rotation.y + PI) * ((mousePos - mouseStart).x * mouseSensitivity)
+			Globals.zw_angle -= sin(rotation.y + PI) * ((mousePos - mouseStart).x * mouseSensitivity)
+			
+			Globals.xw_angle += cos(rotation.y + PI * 0.5) * ((mousePos - mouseStart).y * mouseSensitivity)
+			Globals.zw_angle -= sin(rotation.y + PI * 0.5) * ((mousePos - mouseStart).y * mouseSensitivity)
 			
 			mouseStart = get_viewport().get_mouse_position()
+		
+		if Input.is_action_just_pressed("follow"):
+			following = !following
+			if following:
+				global_position = Globals.physics_objects[follow_id].global_position
+				Globals.camera_w = Globals.physics_objects[follow_id].world_pos.w
+		
+		if Input.is_action_pressed("cycle_backward"):
+			input_timer -= delta
+			if input_timer <= 0.0:
+				follow_id -= 1
+				input_timer = 0.1
+				if following:
+					global_position = Globals.physics_objects[follow_id].global_position
+					Globals.camera_w = Globals.physics_objects[follow_id].world_pos.w
+		
+		if Input.is_action_pressed("cycle_forward"):
+			input_timer -= delta
+			if input_timer <= 0.0:
+				follow_id += 1
+				input_timer = 0.1
+				if following:
+					global_position = Globals.physics_objects[follow_id].global_position
+					Globals.camera_w = Globals.physics_objects[follow_id].world_pos.w
+		
+		if following:
+			var particle_count = Globals.physics_objects.size()
+			if follow_id < 0:
+				follow_id += particle_count
+			if follow_id >= particle_count:
+				follow_id -= particle_count
+			
+			global_position = lerp(global_position, Globals.physics_objects[follow_id].global_position, 1 - pow(2.0, -delta / 0.1))
+			Globals.camera_w = lerp(Globals.camera_w, Globals.physics_objects[follow_id].world_pos.w, 1 - pow(2.0, -delta / 0.1))
