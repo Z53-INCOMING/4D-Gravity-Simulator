41f39659de353b1fc40c34c2a83148b7fa652309
Z53-INCOMING <75651517+Z53-INCOMING@users.noreply.github.com>
2024-06-01 11:17:20 -0700

    fixed bugs

---
+++ FullscreenManager.gd
@@ -0,0 +1,8 @@
+extends Node
+
+func _process(delta):
+	if Input.is_action_just_pressed("fullscreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
